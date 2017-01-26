3273721c8fa68d9905d0bff68eccfb3ecfc458be
root <gerard.ramosm89@gmail.com>
2017-01-26 13:41:52 -0800

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+	divStyle = {
+		'text-align':'center'
+	};
+	render() {
+    return (
+			<div>
+			<div className="jumbotron text-center">
+				<h1>KnowYourGlass Testing Grounds</h1>
+			</div>
+			</div>
+    );
+  }
+}
