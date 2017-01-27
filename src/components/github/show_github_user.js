62a0d7f14c73cbdf49316b0a0cd9bbe4ad4fb8c1
root <gerard.ramosm89@gmail.com>
2017-01-27 10:05:30 -0800

    refactored some github user html

--- src/components/github/show_github_user.js
+++ src/components/github/show_github_user.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import GithubUserHTML from './github_user_html';
 
-export default class ViewGithubUser {
+export default class ViewGithubUser extends Component {
 	render(){
 		return(
-			<div>Here is where the Github user will show </div>
+				<div>
+				<div>Here is where the Github user will show </div>
+				<GithubUserHTML />
+				</div>
 		)
 	}
 }
