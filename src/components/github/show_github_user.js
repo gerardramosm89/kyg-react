43f2612e6d2ce3176bd8b133f786811b4e5e909a
root <gerard.ramosm89@gmail.com>
2017-01-27 09:33:30 -0800

    adding view github user page

---
+++ src/components/github/show_github_user.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ViewGithubUser {
+	render(){
+		return(
+			<div>Here is where the Github user will show </div>
+		)
+	}
+}
--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,13 @@ import App from './components/app';
 import PostsIndex from './components/posts_index';
 import PostsNew from './components/posts_new';
 import NewReview from './components/new_review';
+import ViewGithubUser from './components/github/show_github_user';
+
 export default (
 	<Route path="/" component={App}>
 	<IndexRoute component={PostsIndex} />
 	<Route path="posts/new" component={PostsNew} />
 	<Route path="posts/newreview" component={NewReview} />
+	<Route path="github" component={ViewGithubUser} />
 	</Route>
 )
