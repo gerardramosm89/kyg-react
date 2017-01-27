4d4d2b9fc44a4007b9d41b5891e6e69a5b918323
root <gerard.ramosm89@gmail.com>
2017-01-26 17:23:08 -0800

    adding redux form added html for creating new blog post

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
-
+import { Link } from 'react-router';
 class PostsIndex extends Component {
 	componentWillMount(){
 		console.log("Now would be a good time to call an action creator");	
 		this.props.fetchPosts();
 	}
 	render() {
-	return (
+		return (
+			<div>
         <div className="jumbotron text-center">
-          <h1>KnowYourGlass Testing Grounds</h1>
-        </div>
+          <h1>Testing Grounds</h1>
+				</div>
+
+				<div className="text-xs-right">
+					<Link to="/posts/new" className="btn btn-primary">Add post</Link>
+				</div>
+			</div>
 	)
 	}
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router';
+import { reduxForm } from 'redux-form';
 class PostsNew extends Component {
 	render(){
 		return(
 			<div>
-				Start here to create a post.
+				<Link to="/" className="text-xs-left btn btn-info">
+				Home
+				</Link>
+				<div className="container">
+					<div className="jumbotron text-center">
+					<h2>Start here to create a post.</h2>
+					</div>
+					<form>
+						<div className="form-group">
+						<label>Title</label>
+						<input type="text" className="form-control" />
+						</div>
+            <div className="form-group">
+            <label>Keywords</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+            <label>Date</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+            <label>Title Image</label>
+            <input type="text" className="form-control" />
+            </div>
+            <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" />
+            </div>
+            <div className="form-group">
+            <label>Author</label>
+            <input type="text" className="form-control" />
+            </div>
+					</form>
+				</div>{/* end container*/}
 			</div>
 		);
 	}
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostsNewForm',
+	fields: ['title','keywords','date','title_image','content','author']
+})(PostsNew);
