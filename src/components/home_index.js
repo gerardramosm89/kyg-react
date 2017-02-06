0a8c00a5f4e0bf0731f933290d244bfa374c4dba
root <gerard.ramosm89@gmail.com>
2017-02-05 20:19:14 -0800

    header changes

--- src/components/home_index.js
+++ src/components/home_index.js
@@ -5,16 +5,18 @@ export default class HomeIndex extends Component {
 	render(){
 		return(
 			<div>
-				<div className="jumbotron text-center">
-					<h1>Hi.</h1>
+			<div className="homebanner">
+				<div className="headerbottom text-center">
+					<h1>Hi, my name is Gerard Ramos</h1>
+					<h3>Full Stack Developer</h3>
 				</div>
-				
+			</div>
 				<div className="container" id="aboutmecontainer">
 					<div className="aboutmeleft">
-						
+
 						<p className="aboutme">About me:</p>
 						Testing the left text
-						
+
 					</div>
 					<img className="aboutmeright aboutmeimage" src="https://idealistinvestment.com/images/stacy-22.jpg" />
 				</div>
