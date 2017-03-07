c7c6f9b3b2cfaf789094a53026af27be6ce1e114
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-06 16:24:35 -0800

    starting auth

--- reactServer.js
+++ reactServer.js
@@ -1,16 +1,26 @@
 var express = require('express');
-
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
 //Create the app
 var app = express();
+router(app);
 var path = require('path');
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*'}));
+
 //serve our static files
+const port = process.env.PORT || 8080;
+
 app.use(express.static(__dirname + '/'));
 console.log("dirname is: ", __dirname);
+
 // viewed at http://localhost:8080
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(8080, function(){
-    console.log('Express server is up on port 8080');
+app.listen(port, function(){
+    console.log(`Express server is up on port ${port}`);
 });
