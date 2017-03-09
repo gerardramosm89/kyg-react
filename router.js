a119307d0b37269c282c7edfa335aca564ef4ea1
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-09 09:34:08 -0800

    can make authenticated requests

--- router.js
+++ router.js
@@ -1,8 +1,12 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app) {
-  app.get('/api', function(req, res, next)  {
-    res.send(['waterbottle', 'phone', 'paper']);
+  app.get('/api', requireAuth,  function(req, res, next)  {
+    res.send({ hi: "there" });
   });
   app.post('/api/signup', Authentication.signup);
 };
