503cacadeeba5231ef2dac8b44104a2eaed67c84
gramosubuntu <gerard.ramosm89@gmail.com>
2017-03-15 13:24:44 -0700

    auth based on token existence

--- src/index.js
+++ src/index.js
@@ -7,14 +7,22 @@ import routes from './routes';
 import reducers from './reducers';
 import promise from 'redux-promise';
 import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(
 	promise,
   reduxThunk
 )(createStore);
 
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+// If we have a token, consider the user to be signed in
+if (token) {
+  // Update application state
+  store.dispatch({ type: AUTH_USER });
+}
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
   </Provider>
   , document.getElementById('renderhere'));
